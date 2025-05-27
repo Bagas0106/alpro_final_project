d7659189b1ca78e66e1f7b565842b1e34f24ac5f
Restu Bagas <rbagas608@gmail.com>
2025-05-27 22:44:32 +0700

    final.go

--- final.go
+++ final.go
@@ -2,11 +2,16 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const NMAX int = 100
 
 type tabInt [NMAX]string
+
+var kataPositif = [5]string{"baik", "bagus", "hebat", "ramah", "keren"}
+var kataNegatif = [5]string{"jelek", "buruk", "jahat", "payah", "lemah"}
 
 func menu() {
 	fmt.Println("=============MENU=============")
@@ -14,7 +19,8 @@ func menu() {
 	fmt.Println("2.Tampilkan komentar")
 	fmt.Println("3.Ubah komentar")
 	fmt.Println("4.Hapus komentar")
-	fmt.Println("5.EXIT")
+	fmt.Println("5.analisis sentiment komentar")
+	fmt.Println("6.EXIT")
 	fmt.Println("==============================")
 	fmt.Println("pilih menu: ")
 }
@@ -38,10 +44,13 @@ func main() {
 			ubahKomentar(&a, n)
 		case "4":
 			fmt.Println("Komentar yang ingin dihapus : ")
-			
+			hapusKomentar(&a, &n)
 		case "5":
+			fmt.Println("daftar berapa komentar positif dan negatif")
+
+		case "6":
 			fmt.Println("Terima kasih sudah menggunakan aplikasi kami.")
-			return 
+			return
 		default:
 			fmt.Println("Pilihan anda tidak valid.")
 		}
@@ -50,7 +59,7 @@ func main() {
 
 func tambahKometar(a *tabInt, n *int) {
 	var comment string
-	fmt.Scanln(&comment) 
+	fmt.Scanln(&comment)
 	a[*n] = comment
 	*n++
 	fmt.Println("Komentar ditambahkan.")
@@ -65,16 +74,29 @@ func tampilKomentar(a tabInt, n int) {
 func ubahKomentar(a *tabInt, n int) {
 	tampilKomentar(*a, n)
 	var index int
-	fmt.Print("Masukkan nomor komentar yang ingin diubah: ")
+	fmt.Print("Masukkan nomor berapa yang ingin diubah: ")
 	fmt.Scanln(&index)
-
-	if index < 1 || index > n {
-		fmt.Println("Nomor komentar tidak valid.")
-		return
-	}
 	var newComment string
 	fmt.Print("Masukkan komentar baru: ")
 	fmt.Scanln(&newComment)
 	a[index-1] = newComment
 	fmt.Println("Komentar berhasil diubah.")
+}
+
+func hapusKomentar(a *tabInt, n *int) {
+	tampilKomentar(*a, *n)
+	var Hapus int
+	fmt.Print("Masukkan nomor komentar yang ingin dihapus: ")
+	fmt.Scanln(&Hapus)
+	if Hapus < 1 || Hapus > *n {
+		fmt.Println("Nomor komentar tidak valid.")
+		return
+	}
+	idx := Hapus - 1
+	for i := idx; i < *n-1; i++ {
+		a[i] = a[i+1]
+	}
+	a[*n-1] = ""
+	*n--
+	fmt.Println("Komentar berhasil dihapus.")
 }
